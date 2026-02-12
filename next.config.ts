d393f5dd0bb29259847b737de8aa4e7398a439f6
54nd339 <59565935+54nd339@users.noreply.github.com>
2026-02-12 23:20:18 +0530

    shadcn/ui, libraries init

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,35 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  // webpack: (config) => {
+  //   config.experiments = {
+  //     ...config.experiments,
+  //     asyncWebAssembly: true,
+  //     layers: true,
+  //   };
+  //   return config;
+  // },
+  async redirects() {
+    return [
+      {
+        source: "/sorting",
+        destination: "/sorting/bubble-sort",
+        permanent: true,
+      },
+      {
+        source: "/searching",
+        destination: "/searching/linear-search",
+        permanent: true,
+      },
+    ];
+  },
+  output: "standalone",
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
 };
 
 export default nextConfig;
